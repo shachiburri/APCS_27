d84a992c7e72a62060c14461aac8de8e9d67ddb5
David Neder <88035886+shachiburri@users.noreply.github.com>
2026-09-25 19:45:03 +0000

    Labs, HW, Exercises done

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -1,15 +1,26 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author: David Neder
+ *  Date: 9/24/26
 */
 
 import java.util.Scanner;
-import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner scanner = new Scanner(System.in);
+
+		int randomNumber = (int) (Math.random() * 1000) + 1;
+
+		System.out.print("Pick a number between 1 - 1000: ");
+		int pickedNumber = scanner.nextInt();
+
+		if (pickedNumber == randomNumber) {
+			System.out.println("You guessed the correct number! Congrats!");
+		}
+		else {
+			System.out.println("Your number wasn't the random number. The number was " + randomNumber + ".");
+		}
+
+		scanner.close();
 	}
 }
--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -1,13 +1,57 @@
 /*
- *	Author:
- *  Date:
- *	Collaborator(s): 
+ *	Author: Daivd Neder
+ *  Date: 9/22/26
+ *	Collaborator(s): -
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
+		System.out.println("Welcome to the Fortune Cookie Generator!");
+		Scanner scanner = new Scanner(System.in);
+		int number = (int)(Math.random() * 10);
+		String password = "david7";
+
+		System.out.println();
+		System.out.print("Password: ");
+		String input = scanner.nextLine();
+
+		if (input.equals(password)) {
+			System.out.println("Welcome in.");
+			System.out.println();
+			String fortune = "";
+
+			if (number == 0) fortune = "Big rizz energy is heading your way.";
+			if (number == 1) fortune = "No cap, your hard work is about to pay off.";
+			if (number == 2) fortune = "Main character energy is unlocked today.";
+			if (number == 3) fortune = "Your vibe today is certified immaculate.";
+			if (number == 4) fortune = "Plot twist incoming: it all works out.";
+			if (number == 5) fortune = "You ate that and left zero crumbs.";
+			if (number == 6) fortune = "This is your glow-up era.";
+			if (number == 7) fortune = "The whole squad is rooting for you.";
+			if (number == 8) fortune = "Your aura points are through the roof today.";
+			if (number == 9) fortune = "Slay now, overthink later.";
+
+			System.out.println(fortune);
+		}
+		else {
+			System.out.println("Password incorrect.");
+			System.out.println();
+			String fortune = "";
+
+			if (number == 0) fortune = "A good idea will find you when you least expect it.";
+			if (number == 1) fortune = "Your creativity will open a door today.";
+			if (number == 2) fortune = "Patience will turn a small win into a big one.";
+			if (number == 3) fortune = "A surprise is on its way, so keep your eyes open.";
+			if (number == 4) fortune = "The next step you take will be the right one.";
+			if (number == 5) fortune = "Someone will thank you for your kindness this week.";
+			if (number == 6) fortune = "Your curiosity will lead you somewhere great.";
+			if (number == 7) fortune = "A fresh perspective will solve an old problem.";
+			if (number == 8) fortune = "Your hard work will not go unnoticed.";
+			if (number == 9) fortune = "The best is yet to come.";
+
+			System.out.println(fortune);
+		}
 	}
 }
